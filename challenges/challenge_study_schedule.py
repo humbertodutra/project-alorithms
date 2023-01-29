4c0dede19b8db751820217c54349986542e038d9
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-01-29 14:27:50 -0300

    exercise 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if not permanence_period or not target_time:
+        return None
+
+    for period in permanence_period:
+
+        start, end = period
+        if not isinstance(start, int) or not isinstance(end, int):
+            return None
+        if start <= target_time <= end:
+            count += 1
+    return count
