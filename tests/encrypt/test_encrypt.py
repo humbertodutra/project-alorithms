7ad5956f3046581b6406ad5a5cde7f1b7b2e9bda
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-01-29 14:54:42 -0300

    exer 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(None, 10) or encrypt_message("abc", None)
+    assert encrypt_message("Galo Doido", -9) == "odioD olaG"
+    assert encrypt_message("GaloDoido", 1) == "G_odioDola"
+    assert encrypt_message("GaloDoido", 2) == "odioDol_aG"
