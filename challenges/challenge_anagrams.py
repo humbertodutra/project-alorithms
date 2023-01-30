d1e8e900cf0516542b15532388a936eaefab1827
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-01-30 14:46:36 -0300

    exer 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def have_same_chars(str1, str2):
+    if len(str1) != len(str2):
+        return False
+    char_counts1 = [0] * 26
+    char_counts2 = [0] * 26
+    for i in range(len(str1)):
+        char_counts1[ord(str1[i]) - ord('a')] += 1
+        char_counts2[ord(str2[i]) - ord('a')] += 1
+    return char_counts1 == char_counts2
+
+
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
+    a = have_same_chars(first_string.lower(), second_string.lower())
+    return (first_string.lower(), second_string.lower(),  a)
+
+
