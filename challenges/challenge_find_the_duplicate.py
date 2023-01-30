b639707b60e8f56c878eb974ac2acaf1912900e7
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-01-30 15:23:33 -0300

    exer 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,37 @@
+def is_input_none(nums):
+    return nums is None
+
+
+def is_input_list(nums):
+    return isinstance(nums, list)
+
+
+def has_sufficient_elements(nums):
+    return len(nums) > 1
+
+
+def is_input_positive_integer(num):
+    return isinstance(num, int) and num >= 0
+
+
+def is_valid_input(nums):
+    if is_input_none(nums) or not is_input_list(nums):
+        return False
+    if not has_sufficient_elements(nums):
+        return False
+    for num in nums:
+        if not is_input_positive_integer(num):
+            return False
+    return True
+
+
 def find_duplicate(nums):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if not is_valid_input(nums):
+        return False
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return num
+        seen.add(num)
+    return False
